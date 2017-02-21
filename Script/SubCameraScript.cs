d93c89d129866852186bdb98319b491b18038c5f
YuFukai201 <yu.fukai0201@gmail.com>
2017-02-21 18:16:50 +0900

    照準の色変更処理

---
+++ Script/SubCameraScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SubCameraScript : MonoBehaviour {
+    //TargetIcon
+	public Image CrossHaier;
+
+    public Camera Subcam;
+    
+
+    void Start() 
+    { 
+		
+    }
+   
+	// Update is called once per frame
+	void Update () 
+    {   
+        
+        //unityのcameraスクリプトリファレンスを参考に
+
+        cameraView();
+
+	}
+
+    void cameraView()
+    {
+		var scale = CrossHaier.transform.lossyScale.x;
+       
+            //視界の変更。targetIconと同じに。
+            Subcam.fieldOfView = 20 * scale;
+		//レティクル内に敵が入ったら
+		if (CommonUtility.GameMain.SearchEnemyName == "") 
+		{
+			CrossHaier.color= new Color (255, 255, 255,255);
+
+		} else 
+		{
+			
+			CrossHaier.color= new Color (255, 0, 0,255);
+		}
+    }
+
+}
