5fa22a713d46956cea931eb9f3c104821607188d
YuFukai201 <yu.fukai0201@gmail.com>
2017-02-21 18:10:54 +0900

    Revert "Revert "残弾数のUI表示""
    This reverts commit 13917c40cee9a5c61515268c3e3a6c11b46c60a8.

---
+++ Script/Fade.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour
+{
+
+
+
+    [SerializeField]
+	Image FadePanel;
+
+    //ステージごとのステージ名UI。CommonUtility.StageNumでなんステージ化を拾う
+	public Image[] StageName;
+
+	public Image otu;
+
+    System.Action m_CallBack;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void FadeInEnd()
+    {
+    }
+
+    void FadeOutEnd()
+    {
+        //第～幕を入れる
+        //フラグ管理
+        Debug.Log("Fade out end");
+    }
+
+
+    public void FadeStart(int mode, System.Action CallBack)
+    {
+
+        m_CallBack = CallBack;
+		if (mode == 0) {
+			//引数
+			StartCoroutine (FadeOutAction(FadePanel));
+
+		} else if (mode == 1) {
+			StartCoroutine (FadeInAction (FadePanel));
+		}
+        //~幕のフェードイン
+        else if (mode == 2) {
+			StartCoroutine (FadeInStageName (StageName[CommonUtility.StageNum]));	
+		}
+        //~幕のフェードアウト
+        else if (mode == 3) {
+			StartCoroutine (FadeOutStageName (StageName[CommonUtility.StageNum]));
+		}
+		//お疲れ様表示
+		else if (mode == 4) {
+			StartCoroutine (Clear_UI_Fadein ());
+		}
+
+    }
+
+   
+	IEnumerator FadeOutAction(Image fpanel)
+    {
+        //2秒経つまで毎フレーム中断
+        //yield return new WaitForSeconds(1.5f);
+        while (fpanel.color.a > 0)
+        {
+            float _subData = 1.0f * Time.deltaTime;
+            float _alpha = (float)fpanel.color.a - _subData;
+            if (_alpha < 0)
+                _alpha = 0;
+            fpanel.color = new Color(fpanel.color.r, fpanel.color.g, fpanel.color.b, _alpha);
+            
+            yield return new WaitForSeconds(0.01f);
+        }
+        m_CallBack();
+
+    }
+
+	IEnumerator FadeOutStageName(Image fpanel)
+	{
+		while (fpanel.color.a > 0)
+		{
+			float _subData = 1.0f * Time.deltaTime;
+			float _alpha = (float)fpanel.color.a - _subData;
+			if (_alpha < 0)
+				_alpha = 0;
+			fpanel.color = new Color(fpanel.color.r, fpanel.color.g, fpanel.color.b, _alpha);
+
+			yield return new WaitForSeconds(0.01f);
+		}
+		m_CallBack();
+
+	}
+
+
+    
+	IEnumerator FadeInAction(Image fpanel)
+    {
+        // 1.5秒経つまで毎フレーム中断
+        //yield return new WaitForSeconds(1.0f);
+        while (fpanel.color.a < 1f)
+        {
+            float _subData = 1.0f * Time.deltaTime;
+            float _alpha = (float)fpanel.color.a + _subData;
+            if (_alpha > 1.0f)
+                _alpha = 1.0f;
+            fpanel.color = new Color(fpanel.color.r, fpanel.color.g, fpanel.color.b, _alpha);
+            yield return new WaitForSeconds(0.01f);
+        }
+        m_CallBack();
+    }
+
+    
+	IEnumerator FadeInStageName(Image fpanel)
+    {
+       
+		//yield return new WaitForSeconds(3.0f);	
+        while (fpanel.color.a < 1f)
+        {
+            float _subData = 1.0f * Time.deltaTime;
+            float _alpha = (float)fpanel.color.a + _subData;
+			if (_alpha > 1.0f) 
+				_alpha = 1.0f;
+			fpanel.color = new Color(fpanel.color.r, fpanel.color.g, fpanel.color.b, _alpha);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        m_CallBack();
+    }
+
+	IEnumerator Clear_UI_Fadein()
+	{
+
+		otu.color = new Color (255, 255, 255, 255);
+
+		yield return new WaitForSeconds (10f);
+		CommonUtility.init ();
+		SceneManager.LoadScene ("Title");
+	}
+}
---
+++ Script/RenderEnemy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class RenderEnemy : MonoBehaviour {
+
+    //カメラの視界に入ってるかどうかの判断
+
+    //視界に入ってる敵の名前をとってる
+	[HideInInspector]
+	public System.Action<string> EnemySerchCallback;
+
+  
+    bool isActive = false;
+    void Update()
+    {
+        if (isActive)
+        {
+			if (CommonUtility.GameMain != null) {
+				CommonUtility.GameMain.SearchEnemyName = gameObject.name;
+				EnemySerchCallback (gameObject.name);
+			}
+        }
+        //リセット処理。バグあり
+        else
+        {
+			if (CommonUtility.GameMain != null)
+				CommonUtility.GameMain.SearchEnemyName = "";
+        }
+        isActive = false;
+    }
+   
+    //	MeshRenderを入れないと反応しない
+    void OnWillRenderObject()
+    {
+		//cardbordの下のsecondcameraのtag名
+		if (Camera.current.tag == "subcamera")
+        {
+            Debug.Log("入ってるよ！よ！");
+            isActive = true;
+        }
+    }
+
+}
